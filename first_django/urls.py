79cba88e1db8c04816f5de37a30d92f1ce9c1ae9
sungn <sungn2@lenovo.com>
2018-09-25 17:32:21 +0800

    Add users, login, logout, register.

--- first_django/urls.py
+++ first_django/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'', include('my_app.urls'))
+    path(r'admin/', admin.site.urls),
+    path(r'users/', include('users.urls', namespace='users')),
+    path(r'', include('my_app.urls', namespace='my_app')),
 ]
